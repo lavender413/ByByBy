88c9db4a3b0214a24f96358bc16b9f0c1fc2251d
Yinghao Zhu <yhzhu99@gmail.com>
2021-12-01 17:24:46 +0800

    feat: init lab01 parser

--- main.py
+++ main.py
@@ -1,29 +1,30 @@
 import ply.lex as lex
+import ply.yacc as yacc
 
 # List of token names.   This is always required
 tokens = [
-    'NUMBER',
-    'IDENT'
+    'number',
+    'ident'
 ]
 
 literals = ['=','+','-','*','/', '(',')',';','<','>','{','}',',','[',']']
 
 reserved = {
-    'int' : 'INT',
-    'main': 'MAIN',
-    'return': 'RETURN'
+    'int' : 'int',
+    'main': 'main',
+    'return': 'return'
 }
 
 tokens = tokens+list(reserved.values())
 
 def t_ident_token(t):
     r'[a-zA-Z_][a-zA-Z_0-9]*'
-    t.type = reserved.get(t.value, 'IDENT')
+    t.type = reserved.get(t.value, 'ident')
     return t
 
 print(tokens)
 # A regular expression rule with some action code
-def t_NUMBER(t):
+def t_number(t):
     r'(0[xX][0-9a-fA-F]+)|(0[0-7]+)|(\d+)'
     n = t.value
     if len(n)==1:
@@ -45,7 +46,7 @@ def t_newline(t):
 # A string containing ignored characters (spaces and tabs)
 t_ignore  = ' \t\r'
 
-def t_COMMENT(t):
+def t_comment(t):
     r'(/\*(.|\n)*?\*/)|(\/\/.*)'
     t.lexer.lineno += t.value.count('\n')
     pass
@@ -56,21 +57,39 @@ def t_error(t):
     print("Illegal character '%s'" % t.value[0])
     t.lexer.skip(1)
 
+def p_expression(p):
+    '''
+    expression :  int main '(' ')' '{' return number ';' '}'
+    '''
+    out = \
+"""define dso_local i32 @main(){{
+    ret i32 {} 
+}}""".format(p[7])
+    p[0] = out
+
+# Error rule for syntax errors
+def p_error(p):
+    print("Syntax error in input!")
+
 # Build the lexer
 lexer = lex.lex()
 
 data = '''
-0x124+{}/<
-0X43
-234
-033
-A
-int main
+int main(){
+    return 033 ;3
+}
 '''
 
 lexer.input(data)
-while True:
-    tok = lexer.token()
-    if not tok: 
-        break      # No more input
-    print(tok)
+# while True:
+#     tok = lexer.token()
+#     if not tok: 
+#         break      # No more input
+#     print(tok)
+
+parser = yacc.yacc()
+
+res = parser.parse(data)
+if res == None:
+    exit(-1)
+print(res)
